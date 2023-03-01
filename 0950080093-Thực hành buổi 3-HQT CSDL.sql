8fdbd572b8c702c1772c01da2ed50d6bb7030bbe
minhduc236 <118780716+minhduc236@users.noreply.github.com>
2023-03-01 08:26:26 +0700

    Add files via upload

---
+++ 0950080093-Thực hành buổi 3-HQT CSDL.sql
@@ -0,0 +1,325 @@
+﻿create database QuanLyDeAn
+use QuanLyDeAn
+Create	table NHANVIEN
+(
+	HOVN	nvarchar(15),
+	TENLOT	nvarchar(15),
+	TENNV	nvarchar(15) not null,
+	MANV	char	(9) not null,
+	NGSINH	Datetime,
+	DCHI	nvarchar(30),
+	PHAI	nvarchar(3),
+	LUONG	float,
+	MA_NQL	char(9),
+	PHG	int,
+	constraint PK_NHANVIEN primary key(MANV),
+	
+)
+
+Create	table PHONGBAN
+(
+	TENPHG	nvarchar(15),
+	MAPHG	int not null,
+	TRPHG	char(9),
+	NG_NHANCHUC Datetime
+	constraint PK_PHONGBAN primary key(MAPHG)
+)
+
+
+Create table DIADIEM_PHG
+(
+	MAPHG	int,
+	DIADIEM nvarchar(50)
+	constraint PK_DIADIEM_PHG primary key (MAPHG, DIADIEM)
+)
+
+Create table DEAN
+(
+	TENDA	nvarchar(15),
+	MADA	int,
+	DDIEM_DA nvarchar(50),
+	PHONG	int,
+	constraint PK_DEAN primary key(MADA)
+)
+
+Create table CONGVIEC
+(
+	MADA int,
+	STT  int,
+	TEN_CONG_VIEC nvarchar(50),
+	constraint PK_CONGVIEC primary key (MADA, STT)
+	
+)
+
+Create table PHANCONG
+(
+	MA_NVIEN char(9),
+	MADA	 int,
+	THOIGIAN float,
+	constraint PK_PHANCONG primary key (MA_NVIEN, MADA)
+)
+
+Create	table THANNHAN
+(
+	MA_NVIEN char(9),
+	TENTN	 nvarchar(15),
+	PHAI	 nvarchar(3),
+	NGSINH	Datetime,
+	QUANHE 	nvarchar(15),
+	constraint PK_THANNHAN primary key (MA_NVIEN, TENTN) 
+)
+
+----------------------------------*tao khoa ngoai*---------------------------
+--tao khoa ngoai cho bang NHANVIEN
+alter table NHANVIEN
+add 
+constraint FK_NHANVIEN_NHANVIEN foreign key(MA_NQL) references NHANVIEN(MANV),
+constraint FK_NHANVIEN_PHONGBAN foreign key(PHG) references PHONGBAN(MAPHG)
+--tao khoa ngoai cho bang PHONGBAN
+alter table PHONGBAN
+add
+constraint FK_PHONGBAN_NHANVIEN foreign key(TRPHG) references NHANVIEN(MANV)
+--tao khoa ngoai cho bang DIADIEM_PHG
+alter table DIADIEM_PHG
+add
+constraint FK_DIADIEMPHG_PHONGBAN foreign key(MAPHG) references PHONGBAN(MAPHG)
+--tao khoa ngoai cho bang DEAN
+alter table DEAN
+add
+constraint FK_DEAN_PHONGBAN foreign key(PHONG) references PHONGBAN(MAPHG)
+--tao khoa ngoai cho CONGVIEC
+/*alter table CONGVIEC
+add
+constraint FK_CONGVIEC_DEAN foreign key(MADA) references DEAN(MADA)
+--tao khoa ngoai cho PHANCONG
+alter table PHANCONG
+add
+constraint FK_PHANCONG_CONGVIEC foreign key(MADA, STT) references CONGVIEC(MADA, STT)
+--tao khoa ngoai cho THANNHAN
+alter table THANNHAN
+add
+*/
+alter table PHANCONG
+add
+constraint FK_PHANCONG_DEAN foreign key(MADA) references DEAN(MADA),
+constraint FK_PHANCONG_NHANVIEN foreign key(MA_NVIEN) references NHANVIEN(MANV)
+alter table THANNHAN
+add constraint FK_THANNHAN_NHANVIEN foreign key(MA_NVIEN) references NHANVIEN(MANV)
+--------------------------Chen du lieu -------------------------------------------
+set dateformat dmy
+select * from nhanvien
+delete  nhanvien
+insert into NHANVIEN values(N'Vương', N'Ngọc', N'Quyền', 
+'006', '01/01/1965', N'45 Trưng Vương Hà Nội', N'Nữ', 55000, null, null)
+
+insert into NHANVIEN values(N'Nguyễn', N'Thanh', N'Tùng',
+'005', '20/08/1962', N'222 Nguyễn Văn Cừ TPHCM', N'Nam', 40000, NULL, null) /*006*/
+
+insert into NHANVIEN values(N'Lê', N'Thị', N'Nhàn', 
+'001', '01/02/1967', N'291 Hồ Văn Huê TPHCM', N'Nữ', 43000, NULL, null)/*006*/
+
+insert into NHANVIEN values(N'Nguyễn', N'Mạnh', N'Hùng', 
+'004', '04/03/1967', N'95 Bà Rịa Vũng Tàu', N'Nam', 38000, NULL, null)/*005*/
+
+insert into NHANVIEN values(N'Trần', N'Thanh', N'Tâm', 
+'003', '04/05/1957', N'34 Mai Thị Lựu TPHCM', N'Nam', 25000, NULL, null) /*005*/
+
+insert into NHANVIEN values(N'Bùi', N'Thúy', N'Bư', 
+'007', '11/03/1954', N'332 Nguyễn Thái Học TPHCM', N'Nam', 25000, NULL, null)/*001*/
+
+insert into NHANVIEN values(N'Trần', N'Hồng', N'Quang', 
+'008', '01/09/1967', N'80 Lê Hồng Phong TPHCM', N'Nam', 25000, NULL, null) /*001*/
+
+insert into NHANVIEN values(N'Đinh', N'Bá', N'Tiến', 
+'009', '12/01/1960', N'119 Cống Quỳnh TPHCM', N'Nam', 30000, NULL, null) /*005*/
+
+--update cho nhan vien Dinh Ba tien
+/*book online :
+	UPDATE titles
+    SET ytd_sales = t.ytd_sales + s.qty
+    FROM titles t, sales s
+    WHERE t.title_id = s.title_id
+*/
+select *
+from NHANVIEN
+
+-- nhap du lieu cho PHONGBAN
+
+insert into PHONGBAN values(N'Nghiên Cứu', 5, '005', '22/05/1987')
+insert into PHONGBAN values(N'Diều Hành', 4, '008', '01/01/1985')
+insert into PHONGBAN values(N'Quản Lý', 1, '006', '19/06/1971')
+SELECT * FROM PHONGBAN
+--cap nhat lai du lieu PHONG cho nhan vien
+
+update NHANVIEN
+set    PHG = 5
+where  MANV = '009'
+
+update NHANVIEN
+set    PHG = 5
+where  MANV = '005'
+
+update NHANVIEN
+set    PHG = 4
+where  MANV = '007'
+
+update NHANVIEN
+set    PHG = 4
+where  MANV = '001'
+
+update NHANVIEN
+set    PHG = 5
+where  MANV = '003'
+
+update NHANVIEN
+set    PHG = 5
+where  MANV = '004'
+
+update NHANVIEN
+set    PHG = 4
+where  MANV = '008'
+
+update NHANVIEN
+set    PHG = 1
+where  MANV = '006'
+
+update NHANVIEN
+set    PHG = 5
+where  MANV = '009'
+
+select *
+from NHANVIEN
+
+--cap nhat du lieu cho bang DIADIEM_PHG
+
+insert into DIADIEM_PHG values(1, 'TP HCM')
+insert into DIADIEM_PHG values(4, 'HA NOI')
+insert into DIADIEM_PHG values(5, 'VUNG TAU')
+
+select *
+from DIADIEM_PHG
+
+--cap nhat du lieu cho bang DEAN
+insert into DEAN values(N'Sản phẩm X', 1, 'VUNG TAU', 5)
+insert into DEAN values(N'Sản phẩm Y', 2, 'NHA TRANG', 5)
+insert into DEAN values(N'Sản phẩm Z', 3, 'TP HCM', 5)
+insert into DEAN values(N'Tin học hóa', 10, 'HA NOI', 4)
+insert into DEAN values(N'Cap Quang', 20, 'TP HCM', 1)
+insert into DEAN values(N'Đào Tạo', 30, 'HA NOI', 4)
+
+select *
+from DEAN
+
+--cap nhat du lieu cho bang CONGVIEC
+
+insert into CONGVIEC values(1, 1, N'Thiết kế sản phẩm X')
+insert into CONGVIEC values(1, 2, N'Thử nghiệm sản phẩm X')
+insert into CONGVIEC values(2, 1, N'Sản xuất sản phẩm Y')
+insert into CONGVIEC values(2, 2, N'Quảng cáo sản phẩm Y')
+insert into CONGVIEC values(3, 1, N'Khuyến mãi sản phẩm Z')
+insert into CONGVIEC values(10, 1, N'Tin học hóa nhân sự tiền lương')
+insert into CONGVIEC values(10, 2, N'Tin học hóa phòng kinh doanh')
+insert into CONGVIEC values(20, 1, N'lắp đặt cáp quang')
+insert into CONGVIEC values(30, 1, N'Đào tạo nhân viên maketing')
+insert into CONGVIEC values(30, 2, N'Đào tạo chuyên viên thiết kế')
+
+select *
+from CONGVIEC
+
+--cap nhat du lieu cho bang THANNHAN
+insert into THANNHAN values('005', N'Quang', N'Nữ', '05/04/1976', N'Con gái')
+insert into THANNHAN values('005', N'Khang', N'Nam', '25/10/1973', N'Con trai')
+insert into THANNHAN values('005', N'Dương', N'Nữ', '03/05/1948', N'Vợ chồng')
+insert into THANNHAN values('001', N'Đăng', N'Nam', '19/02/1932', N'Vợ chồng')
+insert into THANNHAN values('009', N'Duy', N'Nam', '01/01/1978', N'Con trai')
+insert into THANNHAN values('009', N'Châu', N'Nữ', '30/12/1978', N'Con gái')
+insert into THANNHAN values('009', N'Phương', N'Nữ', '05/05/1957', N'Vợ chồng')
+
+select *
+from THANNHAN
+
+--chen du lieu cho bang PHANCONG
+insert into PHANCONG values('009', 1, 32)
+insert into PHANCONG values('009', 2, 8)
+insert into PHANCONG values('004', 3, 40)
+insert into PHANCONG values('003', 1, 20)
+insert into PHANCONG values('003', 2, 20)
+insert into PHANCONG values('008', 10,35)
+insert into PHANCONG values('008', 30,5)
+insert into PHANCONG values('001', 30,20)
+insert into PHANCONG values('001', 20,15)
+insert into PHANCONG values('006', 20,30)
+insert into PHANCONG values('005', 3, 10)
+insert into PHANCONG values('005', 10,10)
+insert into PHANCONG values('005', 20,10)
+insert into PHANCONG values('007', 30,30)
+insert into PHANCONG values('007', 10,10)
+
+select *
+from	PHANCONG
+
+-----1. Liệt kê danh sách tất cả các nhân viên
+select *
+from NHANVIEN
+-----2. Tìm các nhân viên làm việc ở phòng số 5
+select MANV, HOVN, TENLOT, TENNV
+from NHANVIEN 
+where PHG = 5
+-----3. Liệt kê họ tên và phòng làm việc các nhân viên có mức lương trên 6.000.000 đồng
+select MANV, HOVN, TENLOT, TENNV, LUONG, PHG
+from NHANVIEN 
+where LUONG > 6000000
+-----4. Tìm các nhân viên có mức lương trên 6.500.000 ở phòng 1 hoặc các nhân viên có mức lương trên 5.000.000 ở phòng 4
+select MANV, HOVN, TENLOT, TENNV, LUONG, PHG
+from NHANVIEN 
+where LUONG > 6500000 and PHG = 1 or LUONG > 5000000 and PHG = 4
+-----5. Cho biết họ tên đầy đủ của các nhân viên ở TP Quảng Ngãi
+SELECT (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' ' + NHANVIEN.TENNV) AS 'Nhân viên ở Quang Ngai'
+	FROM NHANVIEN
+	WHERE NHANVIEN.DCHI LIKE '%QuangNgai'
+-----6. Cho biết họ tên đầy đủ của các nhân viên có họ bắt đầu bằng ký tự 'N'
+SELECT (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' ' + NHANVIEN.TENNV) AS 'Nhân viên có họ bắt đầu bằng "N"'
+	FROM NHANVIEN
+	WHERE NHANVIEN.HONV LIKE 'N%'
+-----7. Cho biết ngày sinh và địa chỉ của nhân viên Cao Thanh Huyền
+SELECT NHANVIEN.NGSINH, NHANVIEN.DCHI
+	FROM NHANVIEN
+	WHERE NHANVIEN.HONV = N'Cao' AND
+		  NHANVIEN.TENLOT = N'Thanh' AND
+		  NHANVIEN.TENNV = N'Huyền'
+-----8. Cho biết các nhân viên có năm sinh trong khoảng 1955 đến 1975
+SELECT NHANVIEN.MANV
+	FROM NHANVIEN
+	WHERE YEAR(NHANVIEN.NGSINH) BETWEEN 1955 AND 1975
+-----9. Cho biết các nhân viên và năm sinh của nhân viên
+SELECT NHANVIEN.MANV, YEAR(NHANVIEN.NGSINH) AS 'Năm sinh'
+	FROM NHANVIEN
+----10. Cho biết họ tên và tuổi của tất cả các nhân viên
+SELECT NHANVIEN.MANV, (YEAR(GETDATE()) - YEAR(NHANVIEN.NGSINH)) AS 'Tuổi'
+	FROM NHANVIEN
+----11. Tìm tên những người trưởng phòng của từng phòng ban
+select HONV+ ' ' +TENLOT+ ' ' +TENNV as 'Họ và tên Trưởng Phòng' from PHONGBAN,NHANVIEN
+where PHONGBAN.TRPHG = NHANVIEN.MANV
+--12.Tìm tên và địa chỉ của tất cả các nhân viên của phòng 'Điều hành'
+select HONV+ ' ' +TENLOT+ ' ' +TENNV as 'Họ và tên', DCHI from NHANVIEN inner join PHONGBAN on NHANVIEN.PHG = PHONGBAN.MAPHG
+where PHONGBAN.MAPHG = 4
+--13. Với mỗi đề án ở Tp Quảng Ngãi, cho biết tên đề án, tên phòng ban, họ tên và ngày nhận chức của trưởng phòng của phòng ban chủ trì đề án đó.
+select TENDEAN, TENPHG, HONV+ ' ' +TENLOT+ ' ' +TENNV as 'Họ và tên', NG_NHANCHUC 
+from NHANVIEN inner join PHONGBAN 
+ON NHANVIEN.PHG = PHONGBAN.MAPHG 
+inner join DEAN ON DEAN.PHONG = PHONGBAN.MAPHG
+where PHONGBAN.TRPHG = NHANVIEN.MANV and DCHI like '%Tp Quảng Ngãi'
+--14. Tìm tên những nữ nhân viên và tên người thân của họ
+select HONV+ ' ' +TENLOT+ ' ' +TENNV as 'Họ và tên', TENTN as 'Tên người thân' 
+from NHANVIEN inner join THANNHAN ON NHANVIEN.MANV = THANNHAN.MA_NVIEN
+where NHANVIEN.PHAI = N'Nữ'
+----17. Cho biết tên các đề án mà nhân viên Trần Thanh Tâm đã tham gia.
+SELECT DEAN.TENDA
+	FROM NHANVIEN, DEAN, PHANCONG
+	WHERE DEAN.MADA	= PHANCONG.MADA AND
+		  NHANVIEN.MANV = PHANCONG.MA_NVIEN AND
+		  NHANVIEN.HONV = N'Trần' AND
+		  NHANVIEN.TENLOT = N'Thanh' AND
+		  NHANVIEN.TENNV = N'Tâm'
+
+
